74fa0899604faf10a3f2d24458bf55804b422107
jlars789 <jlars789@gmail.com>
2022-12-17 13:48:11 -0500

    Visited count

--- main/models.py
+++ main/models.py
@@ -1,4 +1,7 @@
 from django.db import models    #type: ignore
 
-class Visits(models.Model):
+class Visit(models.Model):
     visits = models.IntegerField()
+
+    def __str__(self):
+        return str(self.visits)
--- main/views.py
+++ main/views.py
@@ -1,8 +1,16 @@
 from django.http import HttpResponse    #type: ignore
 from django.shortcuts import render     #type: ignore
+import main.models as models
 
 def index(request):
-    return render(request, "index.html")
+    val = models.Visit.objects.get(id=1)
+    val.visits += 1
+    val.save()
+    context = {
+        "visits": val.visits
+    }
+
+    return render(request, "index.html", context=context)
 
 def testView(request):
     html = "<html><body>This is a test.</body></html>"
