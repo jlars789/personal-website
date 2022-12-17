dff5079ad02a863a70224388d6fb6ce13b4aafd4
jlars789 <jlars789@gmail.com>
2022-12-17 11:34:32 -0500

    Fixing a whoopsie

--- jlars789/urls.py
+++ jlars789/urls.py
@@ -14,8 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from main import views
-from django.contrib import admin
-from django.urls import path
+from django.contrib import admin    #type: ignore
+from django.urls import path        #type: ignore
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse    #type: ignore
+from django.shortcuts import render     #type: ignore
 
 def index(request):
     return render(request, "index.html")
